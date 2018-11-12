c3f7cadb667f48a3e1f6a19ff61864fadad49b6a
flege <abd.alfaruq@gmail.com>
2018-11-12 23:57:07 +0700

    update sql database

--- coffeshop.sql
+++ coffeshop.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
--- http://www.phpmyadmin.net
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2018 at 10:38 AM
--- Server version: 5.6.25
--- PHP Version: 5.6.11
+-- Generation Time: Nov 12, 2018 at 05:56 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,12 +28,19 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
   `id_admin` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama`, `username`, `password`) VALUES
+(1, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -39,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 -- Table structure for table `detail_transaksi`
 --
 
-CREATE TABLE IF NOT EXISTS `detail_transaksi` (
+CREATE TABLE `detail_transaksi` (
   `id_detail_transaksi` int(11) NOT NULL,
   `id_transaksi` int(11) NOT NULL,
   `id_produk` int(11) NOT NULL,
@@ -47,17 +56,36 @@ CREATE TABLE IF NOT EXISTS `detail_transaksi` (
   `harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail_transaksi`, `id_transaksi`, `id_produk`, `jumlah`, `harga`) VALUES
+(1, 4, 1, 2, 44),
+(2, 4, 3, 5, 100);
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `produk`
 --
 
-CREATE TABLE IF NOT EXISTS `produk` (
+CREATE TABLE `produk` (
   `id_produk` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `harga` int(11) NOT NULL
+  `harga` int(11) NOT NULL,
+  `status` enum('1','0') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `nama`, `harga`, `status`) VALUES
+(1, 'hhhhhhhhhhhhhhhhhhh', 12321, '1'),
+(2, 'dsgffdsg', 2132, '1'),
+(3, 'gfdhdfghd', 354, '1'),
+(4, 'hjgvf', 234, '1');
 
 -- --------------------------------------------------------
 
@@ -65,11 +93,20 @@ CREATE TABLE IF NOT EXISTS `produk` (
 -- Table structure for table `shop`
 --
 
-CREATE TABLE IF NOT EXISTS `shop` (
+CREATE TABLE `shop` (
   `id_shop` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `status` enum('1','0') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shop`
+--
+
+INSERT INTO `shop` (`id_shop`, `nama`, `status`) VALUES
+(1, 'asdasd', '1'),
+(2, 'hhgdhdfgr', '1'),
+(3, '467yhjfbh', '1');
 
 -- --------------------------------------------------------
 
@@ -77,13 +114,23 @@ CREATE TABLE IF NOT EXISTS `shop` (
 -- Table structure for table `transaksi`
 --
 
-CREATE TABLE IF NOT EXISTS `transaksi` (
+CREATE TABLE `transaksi` (
   `id_transaksi` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_shop` int(11) NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` enum('0','1','2','3') NOT NULL DEFAULT '0'
+  `status` enum('0','1','2','3','4') NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_user`, `id_shop`, `tanggal`, `status`) VALUES
+(1, 1, 1, '2018-11-12 15:22:27', '1'),
+(2, 2, 2, '2018-11-12 15:29:13', '0'),
+(3, 3, 3, '2018-11-12 15:29:38', '4'),
+(4, 4, 3, '2018-11-12 15:29:38', '2');
 
 -- --------------------------------------------------------
 
@@ -91,7 +138,7 @@ CREATE TABLE IF NOT EXISTS `transaksi` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `id_shop` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
@@ -100,6 +147,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` enum('owner','member') DEFAULT NULL,
   `status` enum('1','0') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `id_shop`, `nama`, `username`, `password`, `role`, `status`) VALUES
+(1, 1, 'saya', 'saya', '20c1a26a55039b30866c9d0aa51953ca', 'owner', '1'),
+(2, 2, 'a', 'a', '0cc175b9c0f1b6a831c399e269772661', 'member', '1'),
+(3, 3, 'q', 'q', '7694f4a66316e53c8cdd9d9954bd611d', 'owner', '1'),
+(4, 1, 'w', 'w', 'f1290186a5d0b1ceab27f4e77c0c5d68', 'member', '1'),
+(5, 2, 'r', 'r', '4b43b0aee35624cd95b910189b3dc231', 'member', '1'),
+(6, 2, 'f', 'f', '8fa14cdd754f91cc6554c9e71929cce7', 'member', '1'),
+(8, 2, 'fk', 'k', '8ce4b16b22b58894aa86c421e8759df3', 'member', '1');
 
 --
 -- Indexes for dumped tables
@@ -139,7 +199,8 @@ ALTER TABLE `transaksi`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -149,32 +210,39 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `shop`
 --
 ALTER TABLE `shop`
-  MODIFY `id_shop` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_shop` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
